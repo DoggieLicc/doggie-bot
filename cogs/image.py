575785a87eba03cb372ceefe1afa86561080bad0
DoggieLicc <doggie@doggieli.cc>
2021-09-16 21:01:38 -0400

    Add pride flag commands

--- cogs/image.py
+++ cogs/image.py
@@ -1,9 +1,9 @@
 import io
 
 import discord
-from PIL import Image, ImageOps, ImageFilter, ImageEnhance, UnidentifiedImageError
+from PIL import Image, ImageOps, ImageFilter, ImageEnhance, UnidentifiedImageError, ImageDraw
 from discord.ext import commands
-from typing import Optional
+from typing import Optional, List
 
 import utils
 
@@ -121,6 +121,45 @@ def maybe_resize_image(image: Image, max_size: int) -> Image:
     return image
 
 
+def make_mask(colors, width, height):
+    color_height = height / len(colors)
+
+    color_image = Image.new('RGBA', (width, height))
+    canvas = ImageDraw.Draw(color_image)
+
+    for i, color in enumerate(colors):
+        x1 = 0
+        x2 = width
+
+        y1 = i * color_height
+        y2 = y1 + color_height
+
+        cords = [(x1, y1), (x2, y2)]
+
+        canvas.rectangle(cords, fill=color)
+
+    return color_image
+
+
+def make_flag(b: bytes, alpha: int, max_size: int, colors: List):
+    if not 0 < alpha <= 100:
+        raise commands.BadArgument('Transparency should be in between 0 and 100')
+
+    alpha /= 100
+
+    image = Image.open(io.BytesIO(b)).convert('RGBA')
+
+    mask = make_mask(colors, *image.size)
+    blended_image = Image.blend(image, mask, alpha)
+    resized_image = maybe_resize_image(blended_image, max_size)
+
+    img_bytes = io.BytesIO()
+    resized_image.save(img_bytes, 'png', optimize=True)
+    img_bytes.seek(0)
+
+    return discord.File(img_bytes, 'pride.png')
+
+
 class Images(commands.Cog):
     """Commands for image manipulation!"""
 
@@ -226,7 +265,7 @@ class Images(commands.Cog):
 
         await ctx.send(embed=embed, file=file)
 
-    @commands.command()
+    @commands.command(aliases=['blurry'])
     async def blur(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], strength=5):
         """Blurs the specified image!
 
@@ -260,7 +299,7 @@ class Images(commands.Cog):
 
         await ctx.send(embed=embed, file=file)
 
-    @commands.command()
+    @commands.command(aliases=['noisy'])
     async def noise(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], strength=50):
         """Adds noise to specified image! Strength should be in between 0 and 100
 
@@ -299,7 +338,7 @@ class Images(commands.Cog):
 
         await ctx.send(embed=embed, file=file)
 
-    @commands.command(aliases=['bright', 'brightness', 'dark', 'darken'])
+    @commands.command(aliases=['bright', 'brightness'])
     async def brighten(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], strength=1.25):
         """Brightens specified image! Passing in an strength less than 1 will darken it instead
 
@@ -369,6 +408,382 @@ class Images(commands.Cog):
             ctx.author,
             title=f'Here\'s your modified image:',
             image='attachment://contrast.png'
+        )
+
+        await ctx.send(embed=embed, file=file)
+
+    @commands.command(aliases=['rainbow'])
+    async def gay(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], transparency=50):
+        """Adds the gay rainbow to image!
+
+        **Steps for getting image:**
+        1. Replied message -> Message steps
+        2. Specified message -> Message steps
+        3. Command's message -> Message steps
+        4. Invoker's avatar
+
+        **Message steps:**
+        1. Attachment
+        2. Sticker
+        3. Embed image/thumbnail
+        3. Specified user
+        4. Specified emote
+        5. Specified link
+        """
+
+        gay_colors = [(255, 0, 24), (255, 165, 44), (255, 255, 65), (0, 128, 24), (0, 0, 249), (134, 0, 125)]
+
+        if not image:
+            image_bytes = await utils.ImageConverter().convert(ctx, image)
+        else:
+            image_bytes = image
+
+        limit = ctx.guild.filesize_limit if ctx.guild else 8 * 1000 * 1000
+        file = await self.bot.loop.run_in_executor(None, make_flag, image_bytes, transparency, limit, gay_colors)
+
+        embed = utils.create_embed(
+            ctx.author,
+            title=f'Here\'s your gay image:',
+            image='attachment://pride.png'
+        )
+
+        await ctx.send(embed=embed, file=file)
+
+    @commands.command(aliases=['transgender'])
+    async def trans(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], transparency=50):
+        """Adds the transgender flag to image!
+
+        **Steps for getting image:**
+        1. Replied message -> Message steps
+        2. Specified message -> Message steps
+        3. Command's message -> Message steps
+        4. Invoker's avatar
+
+        **Message steps:**
+        1. Attachment
+        2. Sticker
+        3. Embed image/thumbnail
+        3. Specified user
+        4. Specified emote
+        5. Specified link
+        """
+
+        trans_colors = [(91, 206, 250), (245, 169, 184), (255, 255, 255), (245, 169, 184), (91, 206, 250)]
+
+        if not image:
+            image_bytes = await utils.ImageConverter().convert(ctx, image)
+        else:
+            image_bytes = image
+
+        limit = ctx.guild.filesize_limit if ctx.guild else 8 * 1000 * 1000
+        file = await self.bot.loop.run_in_executor(None, make_flag, image_bytes, transparency, limit, trans_colors)
+
+        embed = utils.create_embed(
+            ctx.author,
+            title=f'Here\'s your transgender image:',
+            image='attachment://pride.png'
+        )
+
+        await ctx.send(embed=embed, file=file)
+
+    @commands.command(aliases=['bisexual'])
+    async def bi(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], transparency=50):
+        """Adds the bisexual flag to image!
+
+        **Steps for getting image:**
+        1. Replied message -> Message steps
+        2. Specified message -> Message steps
+        3. Command's message -> Message steps
+        4. Invoker's avatar
+
+        **Message steps:**
+        1. Attachment
+        2. Sticker
+        3. Embed image/thumbnail
+        3. Specified user
+        4. Specified emote
+        5. Specified link
+        """
+
+        bi_colors = [(216, 9, 126), (216, 9, 126), (140, 87, 156), (36, 70, 142), (36, 70, 142)]
+
+        if not image:
+            image_bytes = await utils.ImageConverter().convert(ctx, image)
+        else:
+            image_bytes = image
+
+        limit = ctx.guild.filesize_limit if ctx.guild else 8 * 1000 * 1000
+        file = await self.bot.loop.run_in_executor(None, make_flag, image_bytes, transparency, limit, bi_colors)
+
+        embed = utils.create_embed(
+            ctx.author,
+            title=f'Here\'s your bisexual image:',
+            image='attachment://pride.png'
+        )
+
+        await ctx.send(embed=embed, file=file)
+
+    @commands.command(aliases=['lesb'])
+    async def lesbian(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], transparency=50):
+        """Adds the lesbian flag to image!
+
+        **Steps for getting image:**
+        1. Replied message -> Message steps
+        2. Specified message -> Message steps
+        3. Command's message -> Message steps
+        4. Invoker's avatar
+
+        **Message steps:**
+        1. Attachment
+        2. Sticker
+        3. Embed image/thumbnail
+        3. Specified user
+        4. Specified emote
+        5. Specified link
+        """
+
+        lesbian_colors = [
+            (213, 45, 0), (239, 118, 39), (255, 154, 86), (255, 255, 255), (209, 98, 164), (181, 86, 144), (163, 2, 98)
+        ]
+
+        if not image:
+            image_bytes = await utils.ImageConverter().convert(ctx, image)
+        else:
+            image_bytes = image
+
+        limit = ctx.guild.filesize_limit if ctx.guild else 8 * 1000 * 1000
+        file = await self.bot.loop.run_in_executor(None, make_flag, image_bytes, transparency, limit, lesbian_colors)
+
+        embed = utils.create_embed(
+            ctx.author,
+            title=f'Here\'s your lesbian image:',
+            image='attachment://pride.png'
+        )
+
+        await ctx.send(embed=embed, file=file)
+
+    @commands.command(aliases=['asexual'])
+    async def ace(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], transparency=50):
+        """Adds the asexual flag to image!
+
+        **Steps for getting image:**
+        1. Replied message -> Message steps
+        2. Specified message -> Message steps
+        3. Command's message -> Message steps
+        4. Invoker's avatar
+
+        **Message steps:**
+        1. Attachment
+        2. Sticker
+        3. Embed image/thumbnail
+        3. Specified user
+        4. Specified emote
+        5. Specified link
+        """
+
+        ace_colors = [(0, 0, 0), (164, 164, 164), (255, 255, 255), (129, 0, 129)]
+
+        if not image:
+            image_bytes = await utils.ImageConverter().convert(ctx, image)
+        else:
+            image_bytes = image
+
+        limit = ctx.guild.filesize_limit if ctx.guild else 8 * 1000 * 1000
+        file = await self.bot.loop.run_in_executor(None, make_flag, image_bytes, transparency, limit, ace_colors)
+
+        embed = utils.create_embed(
+            ctx.author,
+            title=f'Here\'s your asexual image:',
+            image='attachment://pride.png'
+        )
+
+        await ctx.send(embed=embed, file=file)
+
+    @commands.command(aliases=['pansexual'])
+    async def pan(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], transparency=50):
+        """Adds the pansexual flag to image!
+
+        **Steps for getting image:**
+        1. Replied message -> Message steps
+        2. Specified message -> Message steps
+        3. Command's message -> Message steps
+        4. Invoker's avatar
+
+        **Message steps:**
+        1. Attachment
+        2. Sticker
+        3. Embed image/thumbnail
+        3. Specified user
+        4. Specified emote
+        5. Specified link
+        """
+
+        pan_colors = [(255, 28, 141), (255, 215, 0), (26, 179, 255)]
+
+        if not image:
+            image_bytes = await utils.ImageConverter().convert(ctx, image)
+        else:
+            image_bytes = image
+
+        limit = ctx.guild.filesize_limit if ctx.guild else 8 * 1000 * 1000
+        file = await self.bot.loop.run_in_executor(None, make_flag, image_bytes, transparency, limit, pan_colors)
+
+        embed = utils.create_embed(
+            ctx.author,
+            title=f'Here\'s your pansexual image:',
+            image='attachment://pride.png'
+        )
+
+        await ctx.send(embed=embed, file=file)
+
+    @commands.command(aliases=['nonbinary', 'non-binary', 'non_binary'])
+    async def nb(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], transparency=50):
+        """Adds the non-binary flag to image!
+
+        **Steps for getting image:**
+        1. Replied message -> Message steps
+        2. Specified message -> Message steps
+        3. Command's message -> Message steps
+        4. Invoker's avatar
+
+        **Message steps:**
+        1. Attachment
+        2. Sticker
+        3. Embed image/thumbnail
+        3. Specified user
+        4. Specified emote
+        5. Specified link
+        """
+
+        nb_colors = [(255, 244, 48), (255, 255, 255), (156, 89, 209), (0, 0, 0)]
+
+        if not image:
+            image_bytes = await utils.ImageConverter().convert(ctx, image)
+        else:
+            image_bytes = image
+
+        limit = ctx.guild.filesize_limit if ctx.guild else 8 * 1000 * 1000
+        file = await self.bot.loop.run_in_executor(None, make_flag, image_bytes, transparency, limit, nb_colors)
+
+        embed = utils.create_embed(
+            ctx.author,
+            title=f'Here\'s your non-binary image:',
+            image='attachment://pride.png'
+        )
+
+        await ctx.send(embed=embed, file=file)
+
+    @commands.command(aliases=['nonconforming'])
+    async def gnc(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], transparency=50):
+        """Adds the gender nonconforming flag to image!
+
+        **Steps for getting image:**
+        1. Replied message -> Message steps
+        2. Specified message -> Message steps
+        3. Command's message -> Message steps
+        4. Invoker's avatar
+
+        **Message steps:**
+        1. Attachment
+        2. Sticker
+        3. Embed image/thumbnail
+        3. Specified user
+        4. Specified emote
+        5. Specified link
+        """
+
+        gnc_colors = [
+            (80, 40, 76), (150, 71, 122), (93, 150, 247), (255, 255, 255), (93, 150, 247), (150, 71, 122), (80, 40, 76)
+        ]
+
+        if not image:
+            image_bytes = await utils.ImageConverter().convert(ctx, image)
+        else:
+            image_bytes = image
+
+        limit = ctx.guild.filesize_limit if ctx.guild else 8 * 1000 * 1000
+        file = await self.bot.loop.run_in_executor(None, make_flag, image_bytes, transparency, limit, gnc_colors)
+
+        embed = utils.create_embed(
+            ctx.author,
+            title=f'Here\'s your gnc image:',
+            image='attachment://pride.png'
+        )
+
+        await ctx.send(embed=embed, file=file)
+
+    @commands.command(aliases=['aro'])
+    async def aromantic(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], transparency=50):
+        """Adds the aromantic flag to image!
+
+        **Steps for getting image:**
+        1. Replied message -> Message steps
+        2. Specified message -> Message steps
+        3. Command's message -> Message steps
+        4. Invoker's avatar
+
+        **Message steps:**
+        1. Attachment
+        2. Sticker
+        3. Embed image/thumbnail
+        3. Specified user
+        4. Specified emote
+        5. Specified link
+        """
+
+        aromantic_colors = [(58, 166, 63), (168, 212, 122), (255, 255, 255), (170, 170, 170), (0, 0, 0)]
+
+        if not image:
+            image_bytes = await utils.ImageConverter().convert(ctx, image)
+        else:
+            image_bytes = image
+
+        limit = ctx.guild.filesize_limit if ctx.guild else 8 * 1000 * 1000
+        file = await self.bot.loop.run_in_executor(None, make_flag, image_bytes, transparency, limit, aromantic_colors)
+
+        embed = utils.create_embed(
+            ctx.author,
+            title=f'Here\'s your aromantic image:',
+            image='attachment://pride.png'
+        )
+
+        await ctx.send(embed=embed, file=file)
+
+    @commands.command(aliases=['gq'])
+    async def genderqueer(self, ctx: utils.CustomContext, image: Optional[utils.ImageConverter], transparency=50):
+        """Adds the genderqueer flag to image!
+
+        **Steps for getting image:**
+        1. Replied message -> Message steps
+        2. Specified message -> Message steps
+        3. Command's message -> Message steps
+        4. Invoker's avatar
+
+        **Message steps:**
+        1. Attachment
+        2. Sticker
+        3. Embed image/thumbnail
+        3. Specified user
+        4. Specified emote
+        5. Specified link
+        """
+
+        genderqueer_colors = [(181, 126, 220), (255, 255, 255), (73, 128, 34)]
+
+        if not image:
+            image_bytes = await utils.ImageConverter().convert(ctx, image)
+        else:
+            image_bytes = image
+
+        limit = ctx.guild.filesize_limit if ctx.guild else 8 * 1000 * 1000
+        file = await self.bot.loop.run_in_executor(
+            None, make_flag, image_bytes, transparency, limit, genderqueer_colors
+        )
+
+        embed = utils.create_embed(
+            ctx.author,
+            title=f'Here\'s your genderqueer image:',
+            image='attachment://pride.png'
         )
 
         await ctx.send(embed=embed, file=file)
